6efda34af58534c38f03ca8d2403c8619b881299
Бельченко Андрей Станиславович <belchenko329@guk.local>
2026-04-18 17:00:01 +0300

    Начата разработка функционала по добавлению файлов.

--- app/components/documents/create/CreateFormFilesSection.vue
+++ app/components/documents/create/CreateFormFilesSection.vue
@@ -3,7 +3,7 @@
 import type {DocumentFormModel} from "~/types/documents/document-form-model";
 import type {DocumentFileType} from "#shared/types/contracts/responses/dictionaries/document-file-type";
 
-const model = defineModel<DocumentFormModel>({ required: true })
+const model = defineModel<DocumentFormModel>({required: true})
 
 const props = defineProps<{
   fileTypes: DocumentFileType[]
@@ -23,6 +23,32 @@ const props = defineProps<{
       <template #header>
         Файлы
       </template>
+
+      <div class="h-full min-h-0 pb-0.25 overflow-auto">
+        <UFileUpload
+            layout="grid"
+            multiple
+            :interactive="false"
+            label="Перетащите сюда файлы"
+        >
+
+          <template #files-top="{ open, files }">
+            <div v-if="files?.length" class="mb-2 flex items-center justify-between">
+              <p class="font-bold">Всего: ({{ files?.length }})</p>
+
+              <UButton
+                  icon="i-lucide-plus"
+                  label="Добавить"
+                  color="neutral"
+                  variant="outline"
+                  class="-my-2"
+                  @click="open()"
+              />
+            </div>
+          </template>
+
+        </UFileUpload>
+      </div>
     </UCard>
   </div>
 </template>
